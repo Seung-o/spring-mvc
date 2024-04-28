ad4baf4cf827b0e9f6042a445183ddab22d8baea
seungo <ilkhso@gmail.com>
2024-04-28 09:44:02 +0900

    feat: 요청 매핑 축약형

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.requestmapping;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +12,18 @@ public class MappingController {
     @RequestMapping("/hello-basic") // GET, POST 등 메서드와 상관 없이 모두 매핑
     public String helloBasic() {
         log.info("helloBasic");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET) // GET 메서드만 매핑
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping("/mapping-get-v2") // GET 메서드만 매핑
+    public String mappingGetV2() {
+        log.info("mappingGetV1");
         return "ok";
     }
 }
