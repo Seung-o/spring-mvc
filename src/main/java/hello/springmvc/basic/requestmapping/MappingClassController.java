e0531d30d30547e784b8525768dc5553942e373f
seungo <ilkhso@gmail.com>
2024-05-09 20:49:26 +0900

    feat: 요청 헤더 매핑 컨트롤러

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -3,19 +3,19 @@ package hello.springmvc.basic.requestmapping;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ *
+ * * 회원 목록 조회: GET /users
+ * * 회원 등록: POST /users
+ * * 회원 조회: GET /users/{userId}
+ * * 회원 수정: PATCH /users/{userId}
+ * * 회원 삭제: DELETE /users/{userId}
+ */
 @RestController
 @RequestMapping("/mapping/users")
 @Slf4j
 public class MappingClassController {
-    /**
-     *
-     * * 회원 목록 조회: GET /users
-     * * 회원 등록: POST /users
-     * * 회원 조회: GET /users/{userId}
-     * * 회원 수정: PATCH /users/{userId}
-     * * 회원 삭제: DELETE /users/{userId}
-     */
-
+    
     @GetMapping
     public String users() {
         return "get users";
