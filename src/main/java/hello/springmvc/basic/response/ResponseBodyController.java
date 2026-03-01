cea0d8203a6dfec969b77f10d8e0a35902e934cd
seungo <ilkhso@gmail.com>
2026-03-01 20:11:43 +0900

    feat: 응답 본문 처리 기능 추가

---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,55 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+//@Controller
+//@ResponseBody
+// @RestController는 @Controller + @ResponseBody의 조합으로, 클래스 레벨에 선언하여 해당 클래스의 모든 메서드에 @ResponseBody가 적용됩니다.
+@RestController
+public class ResponseBodyController {
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return ResponseEntity.ok("ok");
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return ResponseEntity.ok(helloData);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+        return helloData;
+    }
+
+}
